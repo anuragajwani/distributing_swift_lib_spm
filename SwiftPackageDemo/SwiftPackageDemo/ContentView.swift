ad57b84230c14f87d6618ee9dc58790b7176e6b3
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-04-26 18:27:51 +0100

    Added SwiftPackageDemo app

---
+++ SwiftPackageDemo/SwiftPackageDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPackageDemo
+//
+//  Created by Anurag Ajwani on 26/04/2020.
+//  Copyright © 2020 Anurag Ajwani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
